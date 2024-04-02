e8f0f65e3e0ac0459af537e54c9c3a28ac918804
vittorms <vitor.msalomao@gmail.com>
2024-04-02 16:50:21 -0300

    Criações das APIs Acessorio, Marca, Cor e Categoria

---
+++ core/models/marca.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.nome.upper()} - {self.id}"
