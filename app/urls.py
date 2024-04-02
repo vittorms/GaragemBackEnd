0183540050aab2b00822555680b1ad23214b23de
vittorms <vitor.msalomao@gmail.com>
2024-04-02 16:49:57 -0300

    Criação das urls

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,15 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"cores", CorViewSet, basename="cores")
+router.register(r"marcas", MarcaViewSet, basename="marcas")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
